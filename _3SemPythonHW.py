11fa68d30c26d6a806e3c151607d6e5935de77e3
Anastasia <79032456178@yandex.ru>
2022-09-15 14:52:06 +0300

    add

--- _3SemPythonHW.py
+++ _3SemPythonHW.py
@@ -22,6 +22,19 @@ print(f'text->{SumOfOddNumbers(list)}')
 #[2, 3, 4, 5, 6] => [12, 15, 16];
 #[2, 3, 5, 6] => [12, 15]
 
+from random import randint
+
+def get_numbers(n, frst, last):
+    return [randint(frst, last) for i in range(n)]
+
+def pairs_mult(numbers):
+    results = []
+    while len(numbers) > 1:
+        results.append(numbers[0]*numbers[-1])
+        del numbers[0] 
+        del numbers[-1] 
+    if len(numbers) ==1: results.append(numbers[0]**2)       
+    return results
 
 
 #3-Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
